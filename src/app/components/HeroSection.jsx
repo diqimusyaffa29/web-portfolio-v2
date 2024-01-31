41836e03642c4bd433b08819eecc6e533b0c0b9e
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2024-01-31 14:24:30 +0800

    Navbar and Hero section is done

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,59 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+
+import {TypeAnimation} from "react-type-animation";
+
+const HeroSection = () => {
+    return (
+        <section>
+            <div className="grid grid-cols-1 sm:grid-cols-12">
+                <div className="col-span-7 place-self-center text-center sm:text-left">
+                    <h1 className="mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+                        <span>Hello, I'm {""}</span>
+                        <br />
+                        <TypeAnimation
+                            sequence={[
+                                "Diqi",
+                                1000,
+                                "Web Developer",
+                                1000,
+                                "Mobile Developer",
+                                1000,
+                                "UI/UX Designer",
+                                1000,
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            repeat={Infinity}
+                        />
+                    </h1>
+                    <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, cupiditate!
+                    </p>
+                    <div>
+                        <button className="px-6 py-3 rounded-full w-full sm:w-fit mr-4 bg-white hover:bg-slate-200 text-black">
+                            Hire me
+                        </button>
+                        <button className="px-6 py-3 rounded-full w-full sm:w-fit mr-4 bg-transparent hover:bg-slate-800 text-white border border-white mt-3">
+                            Download CV
+                        </button>
+                    </div>
+                </div>
+                <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+                    <div className="rounded-full bg-[#181818] w-[300px] h-[300px] lg:w-[500] lg:h-[500] relative">
+                        <Image
+                            src="https://firebasestorage.googleapis.com/v0/b/personal-portfolio-6163a.appspot.com/o/hero-image-removebg-preview.png?alt=media&token=5e60ec7f-64c2-44b3-9408-7ef5c32ebed6"
+                            alt="hero Image"
+                            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                            width={300}
+                            height={300}
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default HeroSection;
